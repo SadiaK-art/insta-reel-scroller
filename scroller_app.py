1bcd341dc5ea61f18d5e5b901534e273ea6dbadb
Sadia Khan <56600010+SadiaK-art@users.noreply.github.com>
2025-04-19 03:21:12 +0600

    Update scroller_app.py

--- scroller_app.py
+++ scroller_app.py
@@ -20,17 +20,16 @@ st.set_page_config(page_title="Sadia's Reel Manager", page_icon="🎬")
 st.markdown(
     """
     <style>
-    body {
-        background-color: #0E1117;
-        color: #FAFAFA;
-    }
     .stApp {
         background-color: #0E1117;
         color: #FAFAFA;
     }
-    header, footer, .css-18ni7ap.e8zbici2 { 
+    .css-18ni7ap.e8zbici2 {
         background-color: #0E1117;
         color: #FAFAFA;
+    }
+    h1, h2, h3, h4, h5, h6, p, label, div, span, input, textarea, button {
+        color: #FAFAFA !important;
     }
     </style>
     """,
